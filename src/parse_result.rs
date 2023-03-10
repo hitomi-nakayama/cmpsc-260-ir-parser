737911e3ea795cff7668efe5b8e358d1f94ed4d5
Hitomi Nakayama <hitominakayama1.618@gmail.com>
2023-03-10 00:34:11 -0800

    Removed ParseError::Unexpected

--- src/parse_result.rs
+++ src/parse_result.rs
@@ -7,8 +7,7 @@ pub enum ParseError {
     Generic(String),
     Expected(usize, String),  // expected but did not find
     ExpectedFound(usize, String, String),  // (Expected, Found)
-    VariableParseError(usize, String),
-    Unexpected(usize, String)  // found but did not expect
+    VariableParseError(usize, String)
 }
 
 impl fmt::Display for ParseError {
@@ -18,8 +17,6 @@ impl fmt::Display for ParseError {
             Generic(msg) => write!(f, "{}", msg),
             Expected(line, expected) => write!(f, "Expected {} on line {}", expected, line),
             ExpectedFound(line, expected, found) => write!(f, "Expected {} but found {} on line {}", expected, found, line),
-            VariableParseError(line, msg) => write!(f, "Variable parse error on line {}: {}", line, msg),
-            Unexpected(line, found) => write!(f, "Unexpected {} on line {}", found, line)
-        }
+            VariableParseError(line, msg) => write!(f, "Variable parse error on line {}: {}", line, msg)        }
     }
 }
