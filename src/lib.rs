1b49d7968be7317d276aba9594f97a5c10ac21e6
Hitomi Nakayama <hitominakayama1.618@gmail.com>
2023-03-13 21:57:54 -0700

    made expect public

--- src/lib.rs
+++ src/lib.rs
@@ -15,7 +15,7 @@ mod variable;
 pub use crate::instruction::{BasicBlockName, Instruction,
     FieldName, FunctionName, Operation, Relation, StructName};
 pub use crate::parse_result::{ParseError, ParseResult};
-pub use crate::parser::{parse, parse_function, parse_variable, parse_list};
+pub use crate::parser::{expect, parse, parse_function, parse_variable, parse_list};
 pub use crate::program::{BasicBlock, BasicBlockId, BasicBlockIdConversionError,
                          Function, FunctionId,
                          InstructionId, InstructionIdConversionError,
