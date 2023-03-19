f27e3e19e6b567d2a96546e2dcb77fdb33b05fd2
Hitomi Nakayama <hitominakayama1.618@gmail.com>
2023-03-18 23:44:54 -0700

    added InstructionRange to pub use.

--- src/lib.rs
+++ src/lib.rs
@@ -18,6 +18,7 @@ pub use crate::parser::{expect, parse, parse_function, parse_variable, parse_lis
 pub use crate::program::{BasicBlock, BasicBlockId, BasicBlockIdConversionError,
                          Function, FunctionId,
                          InstructionId, InstructionIdConversionError,
+                         InstructionRange,
                          Program, Struct};
 pub use crate::text::{create_token_reader, str_to_tokens, TokenReader};
 pub use crate::variable::{BaseType, TypeName, Variable, VariableName, Value};
